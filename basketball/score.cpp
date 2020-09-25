69d1f37125bf1c7ab2c2044fa7b5e8fb77a85bef
upitroma <allen@rudolphweb.com>
2020-09-25 10:30:59 -0400

    works

--- basketball/score.cpp
+++ basketball/score.cpp
@@ -13,6 +13,7 @@ int main(){
 
     char ch;
     int aScore;
+    aScore=0;
     bool isAPoint;
 
     while(!fileIn.eof()){
@@ -21,33 +22,45 @@ int main(){
         if(ch=='\n'){
             if(aScore>1||aScore<-1){
                 if(aScore>1){
-                    cout<<"A\n";
+                    cout<<"A"<<aScore<<"\n";
                 }
                 else{
-                    cout<<"B\n";
+                    cout<<"B"<<aScore<<"\n";
                 }
-            }
-        }
-
-        if(ch=='A'||ch=='B'){
-            isAPoint=ch=='A';
-        }
-        else{
-            if(isAPoint){
-                aScore+=ch;
+                aScore=0;
             }
             else{
-                aScore-=ch;
+                cout<<"invalid input\n";
+            }
+        }
+        else{
+            if(ch=='A'||ch=='B'){
+                isAPoint=ch=='A';
+            }
+            else{
+                if(isAPoint){
+                    aScore+=(ch-'0');
+                }
+                else{
+                    aScore-=(ch-'0');
+                }
+                //cout<<(ch-'0')<<" ";
             }
         }
     }
+
+        
     if(aScore>1||aScore<-1){
         if(aScore>1){
-            cout<<"A\n";
+            cout<<"A"<<aScore<<"\n";
         }
         else{
-            cout<<"B\n";
+            cout<<"B"<<aScore<<"\n";
         }
+        aScore=0;
+    }
+    else{
+        cout<<"invalid input\n";
     }
 
 /*
