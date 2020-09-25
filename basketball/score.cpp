8d75511d9d0a7ce146eb7bf3367734828e58b051
upitroma <allen@rudolphweb.com>
2020-09-25 11:07:15 -0400

    end of file no longer counts as a point

--- basketball/score.cpp
+++ basketball/score.cpp
@@ -5,71 +5,56 @@
 using namespace std;
 
 ifstream fileIn;
-ofstream fileOut;
+int score;
+
+void tallyScores(){
+    if(score>1||score<-1){//win by 2 rule
+        if(score>1){
+            cout<<"A\n";
+        }
+        else{
+            cout<<"B\n";
+        }
+    }
+    else{
+        cout<<"Invalid game: (must win by 2)\n";
+    }
+    score=0;
+}
+
 
 int main(){
 
     fileIn.open("scores.txt");
 
     char ch;
-    int aScore;
-    aScore=0;
+    bool isScore;
     bool isAPoint;
 
     while(!fileIn.eof()){
         fileIn.get(ch);
 
         if(ch=='\n'){
-            if(aScore>1||aScore<-1){
-                if(aScore>1){
-                    cout<<"A"<<aScore<<"\n";
-                }
-                else{
-                    cout<<"B"<<aScore<<"\n";
-                }
-                aScore=0;
-            }
-            else{
-                cout<<"invalid input\n";
-            }
+            tallyScores();
         }
         else{
             if(ch=='A'||ch=='B'){
                 isAPoint=ch=='A';
+                isScore=true;
             }
-            else{
+            else if(isScore){
                 if(isAPoint){
-                    aScore+=(ch-'0');
+                    score+=(ch-'0');
                 }
-                else{
-                    aScore-=(ch-'0');
+                else {
+                    score-=(ch-'0');
                 }
-                //cout<<(ch-'0')<<" ";
+                isScore=false;
             }
         }
     }
-
-        
-    if(aScore>1||aScore<-1){
-        if(aScore>1){
-            cout<<"A"<<aScore<<"\n";
-        }
-        else{
-            cout<<"B"<<aScore<<"\n";
-        }
-        aScore=0;
-    }
-    else{
-        cout<<"invalid input\n";
-    }
-
-/*
-    fstream fin("scores.txt", fstream::in);
-    while (fin >> noskipws >> ch) {
-        //if(ch='\n') 
-    }
- */   
-
+    tallyScores();
 
     return 0;
 }
+
