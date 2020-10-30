3585e509a86871723e70b0823b50c89cf5bb6b27
upitroma <allen@rudolphweb.com>
2020-10-30 09:36:47 -0400

    done

--- searching/searchBubSel.cpp
+++ searching/searchBubSel.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
-#include <cmath>
 
 using namespace std;
 
 const int MAX = 200;
 int newarray[MAX];
+int otherNewArray[MAX];
 void dataGet(int newarray[]);
 void dataPrint(int newarray[]);
 void bubbleSort(int list[], int length);
+void selectionSort(int list[], int length);
 ifstream inFile;
 ofstream outFileBub;
+ofstream outFileSel;
 
 int main ()
    {
       inFile.open("ints.txt");
       outFileBub.open("bubbleOut.txt");
+      outFileSel.open("selectionOut.txt");
       dataGet(newarray);      
+      dataGet(otherNewArray);      
       inFile.close();
 
       bubbleSort(newarray, 200);
+      selectionSort(otherNewArray, 200);
       
       outFileBub.close();
       return 0;
@@ -73,4 +77,50 @@ void bubbleSort(int list[], int length)
 
     cout<<"comparisons: "<<comparisons<<" data moves: "<<dataMoves<<endl;
     outFileBub<<"comparisons: "<<comparisons<<" data moves: "<<dataMoves<<endl;
-} //end bubbleSort
+}
+
+int selectionComparisions=0;
+int selectionMoves=0;
+
+int minLocation(int list[], int first, int last);
+void swap(int list[], int first, int second);
+void selectionSort(int list[], int length)
+{
+   selectionComparisions=0;
+   selectionMoves=0;
+    int loc, minIndex;
+
+    for (loc = 0; loc < length; loc++)
+    {
+        minIndex = minLocation(list, loc, length - 1);
+        swap(list, loc, minIndex);
+    }
+    cout<<"comparisons: "<<selectionComparisions<<" data moves: "<<selectionMoves<<endl;
+    outFileSel<<"comparisons: "<<selectionComparisions<<" data moves: "<<selectionMoves<<endl;
+}
+
+
+void swap(int list[], int first, int second)
+{
+    int temp;
+
+    temp = list[first];
+    list[first] = list[second];
+    list[second] = temp;
+    selectionMoves+=3;
+}
+int minLocation(int list[], int first, int last)
+{
+    int loc, minIndex;
+
+    minIndex = first;
+
+    for (loc = first + 1; loc <= last; loc++){
+       selectionComparisions++;
+    
+        if (list[loc] < list[minIndex])
+            minIndex = loc;
+    }
+
+    return minIndex;
+}
