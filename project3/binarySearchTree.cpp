9f9953d109b0d679f68b9dc0a307f38ea5cb7415
Rudolph <jarudolph01@zec108d-imac20.cs.indianatech.net>
2020-11-06 09:51:54 -0500

    binary search tree works

---
+++ project3/binarySearchTree.cpp
@@ -0,0 +1,60 @@
+//https://www.techiedelight.com/insertion-in-bst/
+#include <iostream>
+
+using namespace std;
+
+class Node{
+public:
+    Node* left;
+    Node* right;
+    int data;
+};
+
+
+Node* insertNode(Node* root, int key)
+{
+	// if the root is null, create a new node and return it
+	if (root == nullptr){
+        Node* n = new Node;
+        n->left=nullptr;
+        n->right=nullptr;
+        n->data=key;
+        return n;
+    }
+
+	else if (key < root->data){
+		root->left = insertNode(root->left, key);
+    }
+
+	else{
+		root->right = insertNode(root->right, key);
+    }
+
+	return root;
+}
+
+void inorder(Node* root)
+{
+	if (root == nullptr)
+		return;
+
+	inorder(root->left);
+	cout << root->data << " ";
+	inorder(root->right);
+}
+
+
+int main(){
+
+    Node* tree=nullptr;
+
+    tree=insertNode(tree, 5);
+    tree=insertNode(tree, 3);
+    tree=insertNode(tree, 1);
+    tree=insertNode(tree, 7);
+    tree=insertNode(tree, 7);
+    inorder(tree);
+
+
+    return 0;
+}
