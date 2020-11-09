9c1c2a2a2c8de6dcfc4a9760b74e788760407d42
Rudolph <jarudolph01@zec108d-imac20.cs.indianatech.net>
2020-11-09 09:26:38 -0500

    added preorder and postorder

--- project3/binarySearchTree.cpp
+++ project3/binarySearchTree.cpp
@@ -37,11 +37,27 @@ void inorder(Node* root)
 	if (root == nullptr){
         return;
     }
-		
-
 	inorder(root->left);
 	cout << root->data << " ";
 	inorder(root->right);
+}
+void preorder(Node* root)
+{
+	if (root == nullptr){
+        return;
+    }
+	cout << root->data << " ";
+    inorder(root->left);
+	inorder(root->right);
+}
+void postorder(Node* root)
+{
+	if (root == nullptr){
+        return;
+    }
+    inorder(root->left);
+	inorder(root->right);
+    cout << root->data << " ";
 }
 
 
@@ -54,7 +70,7 @@ int main(){
     tree=insertNode(tree, 1);
     tree=insertNode(tree, 7);
     tree=insertNode(tree, 7);
-    inorder(tree);
+    postorder(tree);
 
 
     return 0;
