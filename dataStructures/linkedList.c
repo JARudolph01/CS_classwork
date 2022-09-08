8e75c51f5f3eb600979873552489b872edab10f9
upitroma <55750919+upitroma@users.noreply.github.com>
2022-09-08 15:56:44 +0000

    frontend

--- dataStructures/linkedList.c
+++ dataStructures/linkedList.c
@@ -92,28 +92,88 @@ void search(int key){
     }
 }
 
-
-int main(){
-    insert(5,1);
-    insert(6,2);
-    insert(7,3);
-    insert(8,4);
-
-    print(head);
-    printf("\n");
-
-
-    insertAfterKey(8,9,100);
-    print(head);
-
-    printf("\n");
-
-    delete(8);
-    print(head);
-    printf("\n");
-
-    search(7);
+void print_length(){
+    struct node *current = head;
+    int c=0;
+    while(current!=NULL){
+        c++;
+        current=current->next;
+    }
+    printf("%d\n",c);
+}
 
 
-    return 0;
+// int main(){
+//     // insert(5,1);
+//     // insert(6,2);
+//     // insert(7,3);
+//     // insert(8,4);
+
+//     // print(head);
+//     // printf("\n");
+
+
+//     // insertAfterKey(8,9,100);
+//     // print(head);
+
+//     // printf("\n");
+
+//     // delete(8);
+//     // print(head);
+//     // printf("\n");
+
+//     // search(7);
+
+int main()
+{
+ int ch = 0;
+
+ while(ch !=6) {
+ int K, k, d;
+ printf("1- Print list\n");
+ printf("2- Print list length\n");
+ printf("3- Insert\n");
+ printf("4- Delete\n");
+ printf("5- Search\n");
+ printf("6- Exit\n");
+ printf("\nYour choice: ");
+ scanf("%d", &ch);
+
+ if(ch == 1) {
+ print();
+ } else if(ch == 2) {
+ print_length();
+ } else if(ch == 3) {
+ int ch2 = 0;
+
+ printf("1- Insert first\n");
+ printf("2- Insert after key\n");
+ printf("\nYour choice: ");
+ scanf("%d", &ch2);
+
+ printf("\nEnter key:");
+ scanf("%d", &k);
+
+ printf("\nEnter data:");
+ scanf("%d", &d);
+
+ if(ch2 == 1) {
+ insert(k, d);
+ } else if(ch2 == 2) {
+ scanf("%d", &K);
+ insertAfterKey(K,k,d);
+ }
+
+ } else if(ch == 4) {
+ printf("\nEnter key:");
+ scanf("%d", &k);
+ delete(k);
+ } else if(ch == 5) {
+ printf("\nEnter key:");
+ scanf("%d", &k);
+ search(k);
+ }
+ }
+
+ return 0;
 }
