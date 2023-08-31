3e15b9cf652d1f8f6654281b62c75c3caa561e6f
Allen Rudolph <55750919+upitroma@users.noreply.github.com>
2023-08-31 16:40:49 +0000

    add a submodule

--- orgOfProgrammingLanguages/hello-world.rs
+++ orgOfProgrammingLanguages/hello-world.rs
@@ -1,3 +1,38 @@
 fn main() {
     println!("Hello World!");
+
+    println!("enter your name: ");
+    
+    let name = user_input();
+    println!("{}",name);
+
+    println!("{}",add(1,-2));
+
+    
 }
+
+fn add(a: i64, b: i64) -> i64 {
+    a + b
+}
+
+
+fn user_input () -> String {
+    use std::io;
+    let mut user_in = String::new();
+    io::stdin().read_line(&mut user_in).expect("failed to readline");
+    user_in
+}
+
+// fn fizz_buzz() {
+//     for x in 1..=100 {
+//         if x % 15 == 0 {
+//             println!("FizzBuzz")
+//         } else if x % 3 == 0 {
+//             println!("Fizz")
+//         } else if x % 5 == 0 {
+//             println!("Buzz")
+//         } else {
+//             println!("{}", x)
+//         }
+//     }
+// }
