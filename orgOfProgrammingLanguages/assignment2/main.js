2c8dc0441ad83c88bc535665832cb21bb07ba9d7
Allen Rudolph <55750919+upitroma@users.noreply.github.com>
2023-10-05 17:11:32 +0000

    kinda works

--- orgOfProgrammingLanguages/assignment2/main.js
+++ orgOfProgrammingLanguages/assignment2/main.js
@@ -192,68 +192,65 @@ console.log(epsilonClosure(13, NFA));
 //part 3
 //convert the NFA to DFA by skipping over epsilon transitions
 
+function arraysEqual(a1,a2) {
+    /* WARNING: arrays must not contain {objects} or behavior may be undefined */
+    return JSON.stringify(a1)==JSON.stringify(a2);
+}
 
 function convertNFAtoDFA(nfa) {
     var DFA = [];
     DFA.push(nfa[1]);
 
+    var i = 0;
+    while (i<DFA.length) {
 
-    //while paths contains epsilon transitions
-    oldpaths = []
-
-
-    function arraysEqual(a1,a2) {
-        /* WARNING: arrays must not contain {objects} or behavior may be undefined */
-        return JSON.stringify(a1)==JSON.stringify(a2);
-    }
-
-
-    while ( !arraysEqual(DFA[0].paths,oldpaths)) {
-        
-        // console.log("DFA[0]: ")
-        // console.log(DFA[0].paths)
-        // console.log("oldpaths: ")
-        // console.log(oldpaths)
-
-        //copy value of DFA[0].paths, not refrence
+        //while paths contains epsilon transitions
         oldpaths = []
-        DFA[0].paths.forEach(function(path) {
-            oldpaths.push(path);
+
+        while ( !arraysEqual(DFA[i].paths,oldpaths)) {
+            //copy value of DFA[0].paths, not refrence
+            oldpaths = []
+            DFA[i].paths.forEach(function(path) {
+                oldpaths.push(path);
+            });
+
+            // console.log(oldpaths==DFA[0].paths)
+
+            DFA[i].paths.forEach(function(path) {
+                if(path.data == 'ε') {
+                    // add the paths of the next node, if not already present
+                    NFA[path.id].paths.forEach(function(nextPath) {
+
+                        if (DFA[i].paths.indexOf(nextPath) == -1) {
+                            console.log("added path: "+nextPath.id)
+                            DFA[i].paths.push(nextPath);
+                        }
+                    });
+                }       
+            });  
+        }
+
+        //now we can remove all epsilon transitions
+        DFA[i].paths.forEach(function(path) {
+            if(path.data == 'ε') {
+                DFA[i].paths = DFA[i].paths.filter(function(e) { return e.data != 'ε' })
+            }
         });
 
-        // console.log(oldpaths==DFA[0].paths)
 
-        DFA[0].paths.forEach(function(path) {
-            if(path.data == 'ε') {
-                // add the paths of the next node, if not already present
-                NFA[path.id].paths.forEach(function(nextPath) {
-
-                    if (DFA[0].paths.indexOf(nextPath) == -1) {
-                        console.log("added path: "+nextPath.id)
-                        DFA[0].paths.push(nextPath);
-                    }
-                });
-            }       
-        });  
-
-        // console.log("DFA[0]: ")
-        // console.log(DFA[0].paths)
-        // console.log("oldpaths: ")
-        // console.log(oldpaths)
+        console.log("DFA["+i+"]");
+        console.log(DFA[i]);
+        i++;
+        
+        //add paths to DFA, unless already present
+        DFA[i-1].paths.forEach(function(path) {
+            if (DFA.indexOf(NFA[path.id]) == -1) {
+                DFA.push(NFA[path.id]);
+            }
+        });
     }
 
-    //now we can remove all epsilon transitions
-    DFA[0].paths.forEach(function(path) {
-        if(path.data == 'ε') {
-            // DFA[0].paths.splice(DFA[0].paths.indexOf(path),1);
-            DFA[0].paths = DFA[0].paths.filter(function(e) { return e.data != 'ε' })
-        }
-    });
-
-
-
-   
-    console.log(DFA[0].paths);
+    // console.log(DFA[i].paths);
 }
 
 
