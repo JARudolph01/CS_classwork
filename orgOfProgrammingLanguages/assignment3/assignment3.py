e5b983c29c8a352d8c220605957a8e13e93a6ee0
Allen Rudolph <55750919+upitroma@users.noreply.github.com>
2023-11-02 16:50:29 +0000

    cleaner

--- orgOfProgrammingLanguages/assignment3/assignment3.py
+++ orgOfProgrammingLanguages/assignment3/assignment3.py
@@ -19,43 +19,40 @@ grammar=[
     ['F','(','E',')'],
     ['F','i']
 ]
-
-input_code = 'i*i$'
+inputCode = 'i*i$'
 
 # table parsing algorithm
-
 S = ['$','P']
-c=input_code[0]
-
+c=inputCode[0]
 while len(S):
-    c=input_code[0]
+    c=inputCode[0]
 
-    print(str((S[::-1]))+"\t"+str(input_code)+"\t",end="")
+    print(str((S[::-1]))+"   "+str(inputCode)+"   ",end="")
+
     X = S[-1]
     if X==c:
         S.pop()
-        input_code=input_code[1:]
-        
+        inputCode=inputCode[1:]
     elif X in terminals:
         print('error, terminal on stack')
         quit()
     elif parseTable[non_terminals.index(X)][terminals.index(c)]!=0:
         S.pop()
 
-        # push symbols alpha on to the stack and repeat
+        # get alpha
         alpha = grammar[parseTable[non_terminals.index(X)][terminals.index(c)]-1][1:]
+
+        # print rule
         print(" "+str(grammar[parseTable[non_terminals.index(X)][terminals.index(c)]-1]))
         
-
+        # push alpha in reverse order
         alpha=alpha[::-1]
         for symbol in alpha:
             if symbol!='ε':
                 S.append(symbol)
-        
-        # print("next is alpha")
-        # print (alpha)
+
     elif parseTable[non_terminals.index(X)][terminals.index(c)]==0:
-        print('error, no entry in parse table. C=',c,'X=',X,'S=',S,'input=',input_code)
+        print('error, no entry in parse table.')
         quit()
     
 print()
